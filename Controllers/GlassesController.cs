d30fddc7dcbd9527cf940b0461bdca69831ea58a
Sam Freeman <samcfreeman888@gmail.com>
2022-10-25 20:48:17 -0500

    CRUD done except PUT

--- Controllers/GlassesController.cs
+++ Controllers/GlassesController.cs
@@ -18,10 +18,53 @@ public ActionResult<List<Glasses>> GetAll() =>
     GlassesService.GetAll();
 
     // GET by Id action
+    [HttpGet("{id}")]
+public ActionResult<Glasses> Get(int id)
+{
+    var glasses = GlassesService.Get(id);
+
+    if(glasses == null)
+        return NotFound();
+
+    return glasses;
+}
 
     // POST action
-
+[HttpPost]
+public IActionResult Create(Glasses glasses)
+{            
+     GlassesService.Add(glasses);
+    return CreatedAtAction(nameof(Create), new { id = glasses.Id }, glasses);
+}
     // PUT action
 
+[HttpPut("{id}")]
+public IActionResult Update(int id, Glasses glasses)
+{
+     if (id != glasses.Id)
+        return BadRequest();
+           
+    var existingGlasses = GlassesService.Get(id);
+    if(existingGlasses is null)
+        return NotFound();
+   
+    GlassesService.Update(glasses);           
+   
+    return NoContent();
+    
+}
+
     // DELETE action
+[HttpDelete("{id}")]
+public IActionResult Delete(int id)
+{
+     var glasses = GlassesService.Get(id);
+   
+    if (glasses is null)
+        return NotFound();
+       
+    GlassesService.Delete(id);
+   
+    return NoContent();
+}
 }
